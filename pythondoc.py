a8d9eb1eaa9f1e7852e35b89b233ad17c2e401be
Rinkusan <59068408+Rinkusan@users.noreply.github.com>
2020-01-15 22:19:41 +0100

    First little Python thing :)

--- pythondoc.py
+++ pythondoc.py
@@ -1,3 +1,20 @@
-import pymongo
-
-
+# What do I even want to do? Maybe a simple RPG basis.
+# Let's say we are in a Sci-Fi Setting, define some Character Types.
+CharTypes = ("Human", "Alien", "Robot","Mortal Hybrid", "Immortal Hybrid", "Android", "Cyborg")
+# Character Creation Function
+def CharCreation():
+    global CharName
+    CharName = input("Enter Character Name:")
+    global CharType
+    CharType = input("What are you? ")
+    if CharType in CharTypes: print("Thank you, " + CharName + ", you are now a Level 1 " + CharType + "!"),
+    else: CharCreation()
+CharCreation()
+CharLevel = 1
+# And our first Character is done.
+Character = {
+    "CharName": CharName,
+    "CharType": CharType,
+    "CharLevel": CharLevel,
+}
+print(Character)
