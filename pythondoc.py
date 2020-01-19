74af3c2f1699b451b4eaa3aeef97ba37b30655a5
Rinkusan <59068408+Rinkusan@users.noreply.github.com>
2020-01-19 23:09:50 +0100

    Tested Some Shit

--- pythondoc.py
+++ pythondoc.py
@@ -1,18 +1,29 @@
 # What do I even want to do? Maybe a simple RPG basis.
 # Let's say we are in a Sci-Fi Setting, define some Character Types.
 CharTypes = ("Human", "Alien", "Robot","Mortal Hybrid", "Immortal Hybrid", "Android", "Cyborg")
+# Level 1 
+global CharLevel
+CharLevel = 1
 # Character Creation Function
 def CharCreation():
     global CharName
     CharName = input("Enter Character Name:")
     global CharType
     CharType = input("What are you? ")
-    if CharType in CharTypes: print("Thank you, " + CharName + ", you are now a Level 1 " + CharType + "!"),
+    if CharType in CharTypes: print("Thank you, " + CharName + ", you are now a Level " + str(CharLevel) + " " + CharType + "!"),
     else:
         print("Sorry, that is not a valid thing to be!")
         CharCreation()
 CharCreation()
-CharLevel = 1
+# And our first Character is done.
+Character = {
+    "CharName": CharName,
+    "CharType": CharType,
+    "CharLevel": CharLevel,
+}
+print(Character)
+# Level Up Funcion
+CharLevel += 1
 # And our first Character is done.
 Character = {
     "CharName": CharName,
